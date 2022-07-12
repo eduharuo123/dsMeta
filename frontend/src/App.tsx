7e88353e4530e0448581e4152e81d5f6f5e38be0
Eduardo Haruo Fuzita Nagata <edunagata123@gmail.com>
2022-07-12 09:34:16 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
 import NotificationButton from "./Components/NotificationButton"
-
+import Header from "./Components/Header"
+import SalesCard from "./Components/SalesCard"
 
 function App() {
 
   return (
     <>
-      <h1>Olá mundo</h1>
-      <NotificationButton/>
-      <NotificationButton/>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
     </>
-    )
+  )
 }
 
 export default App
---
+++ frontend/src/Components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from "../../assets/img/logo.svg"
+import './styles.css'
+
+
+function Header() {
+
+    return (
+        <header>
+            <div className="LogoContainer">
+                <img src={logo} alt="logo DsMeta" />
+                    <h1>DsMeta</h1>
+                    <p>Desenvolvido por
+                        <a href="">@devsuperior.ig</a>
+                    </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
