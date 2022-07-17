048e5e7db4810b5ed947de83222d6c028df052eb
Eduardo Haruo Fuzita Nagata <edunagata123@gmail.com>
2022-07-17 15:53:56 -0300

    Sale listing

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -32,8 +32,7 @@ public class SmsService {
 
 		Sale sale = saleRepository.findById(saleId).get();
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
-		String msg = "Vendedor " + sale.getSellerName() + "foi destaque em" + date 
-				+ " com um total de R$ " + String.format("%.2f", sale.getAmount());	
+		String msg = "Vendedor " + sale.getSellerName() + "foi destaque em" + date + " com um total de R$ " + String.format("%.2f", sale.getAmount());	
 
 		Twilio.init(twilioSid, twilioKey);
 
---
+++ frontend/src/models/Sale.ts
@@ -0,0 +1,8 @@
+export type Sale = {
+    date: string;
+    id: number;
+    sellerName: string;
+    visited: number
+    deals: number;
+    amount: number;
+}
