6da716737ba485aae07e419a19397827de914852
Eduardo Haruo Fuzita Nagata <edunagata123@gmail.com>
2022-07-17 17:24:07 -0300

    Send notification

--- frontend/src/Components/NotificationButton/index.tsx
+++ frontend/src/Components/NotificationButton/index.tsx
@@ -1,9 +1,22 @@
+import axios from "axios";
 import icon from "../../assets/img/NotificationIcon.svg";
+import { BASE_URL } from "../../utils/request";
 import './styles.css';
 
-function NotificationButton() {
+type props = {
+    saleId: number;
+}
+
+function handleClick(id: any) {
+    axios(`${BASE_URL}/sales/${id}/notification`)
+        .then(response => {
+            console.log('sucesso')
+        });
+}
+
+function NotificationButton( {saleId} : props) {
     return(
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
